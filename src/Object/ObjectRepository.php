f22d13246fa35bccdb99e03dbd52442b539ae579
Timon Amstutz <timon.amstutz@bluewin.ch>
2017-11-27 12:50:11 +0100

    Fix db error for empty arrays

--- src/Object/ObjectRepository.php
+++ src/Object/ObjectRepository.php
@@ -55,14 +55,26 @@ abstract class ObjectRepository implements IObjectRepository {
 	public function getToDelete(array $ext_ids) {
 		$class = $this->getClass();
 
-		/** @var $class \ActiveRecord */
-		return $class::where([
-			'origin_id' => $this->origin->getId(),
-			// We only can transmit from final states CREATED and UPDATED to TO_DELETE
-			// E.g. not from DELETED or IGNORED
-			'status'    => [ IObject::STATUS_CREATED, IObject::STATUS_UPDATED ],
-			'ext_id'    => $ext_ids,
-		], [ 'origin_id' => '=', 'status' => 'IN', 'ext_id' => 'NOT IN' ])->get();
+		if(count($ext_ids)> 0){
+			/** @var $class \ActiveRecord */
+			return $class::where([
+					'origin_id' => $this->origin->getId(),
+				// We only can transmit from final states CREATED and UPDATED to TO_DELETE
+				// E.g. not from DELETED or IGNORED
+					'status'    => [ IObject::STATUS_CREATED, IObject::STATUS_UPDATED ],
+					'ext_id'    => $ext_ids,
+			], [ 'origin_id' => '=', 'status' => 'IN', 'ext_id' => 'NOT IN' ])->get();
+		}
+		else{
+			/** @var $class \ActiveRecord */
+			return $class::where([
+					'origin_id' => $this->origin->getId(),
+				// We only can transmit from final states CREATED and UPDATED to TO_DELETE
+				// E.g. not from DELETED or IGNORED
+					'status'    => [ IObject::STATUS_CREATED, IObject::STATUS_UPDATED ]
+			], [ 'origin_id' => '=', 'status' => 'IN'])->get();
+		}
+
 	}
 
 
