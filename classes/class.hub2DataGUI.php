9071f60216838643cfd22c835490dc86d557f20d
Fabian Schmid <fs@studer-raimann.ch>
2018-07-20 10:19:28 +0200

    [FIX] ILUB-132 Part I

--- classes/class.hub2DataGUI.php
+++ classes/class.hub2DataGUI.php
@@ -112,13 +112,13 @@ class hub2DataGUI extends hub2MainGUI {
 		ksort($filtered);
 
 		// Unfortunately the item suchs in rendering in Modals, therefore we take a descriptive listing
-		$data_table = $factory->item()->standard(sprintf($this->pl->txt("table_ext_id"), $object->getExtId()))->withProperties($filtered);
+		$data_table = $factory->item()->standard(sprintf($this->pl->txt("data_table_ext_id"), $object->getExtId()))->withProperties($filtered);
 
 		$data_table = $factory->listing()->descriptive($filtered);
 
 		$renderer = $this->ui()->renderer();
 
-		$modal = $factory->modal()->roundtrip(sprintf($this->pl->txt("table_hash"), $object->getHashCode()), $data_table);
+		$modal = $factory->modal()->roundtrip(sprintf($this->pl->txt("data_table_hash"), $object->getHashCode()), $data_table);
 
 		echo $renderer->renderAsync($modal);
 		exit;
