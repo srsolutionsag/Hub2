6c725c90e6845569dc3cf95961e9d3c2b676afee
Florian <fw@studer-raimann.ch>
2018-08-28 14:24:25 +0200

    DICTrait

--- src/Sync/Processor/User/UserSyncProcessor.php
+++ src/Sync/Processor/User/UserSyncProcessor.php
@@ -90,10 +90,10 @@ class UserSyncProcessor extends ObjectSyncProcessor implements IUserSyncProcesso
 
 	protected function handleCreate(IDataTransferObject $dto) {
 		/** @var UserDTO $dto */
-		$ilObjUser = new \ilObjUser();
+		$ilObjUser = new ilObjUser();
 
-		$user_login =  \ilObjUser::_checkExternalAuthAccount($dto->getAuthMode(),$dto->getExternalAccount(),false);
-		$user_id = \ilObjUser::_loginExists($user_login);
+		$user_login =  ilObjUser::_checkExternalAuthAccount($dto->getAuthMode(),$dto->getExternalAccount(),false);
+		$user_id = ilObjUser::_loginExists($user_login);
 
 		if($user_id){
 			return $this->handleUpdate($dto,$user_id);
