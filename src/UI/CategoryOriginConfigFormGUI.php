d6841f0a32fe8f4095463e1b021b23134f86386e
Florian <fw@studer-raimann.ch>
2018-09-03 10:31:20 +0200

    DIC PluginInterface

--- src/UI/CategoryOriginConfigFormGUI.php
+++ src/UI/CategoryOriginConfigFormGUI.php
@@ -28,13 +28,13 @@ class CategoryOriginConfigFormGUI extends OriginConfigFormGUI {
 	protected function addSyncConfig() {
 		parent::addSyncConfig();
 
-		$te = new ilTextInputGUI(self::translate('cat_prop_base_node_ilias'), $this->conf(ICategoryOriginConfig::REF_ID_NO_PARENT_ID_FOUND));
-		$te->setInfo(self::translate('cat_prop_base_node_ilias_info'));
+		$te = new ilTextInputGUI(self::plugin()->translate('cat_prop_base_node_ilias'), $this->conf(ICategoryOriginConfig::REF_ID_NO_PARENT_ID_FOUND));
+		$te->setInfo(self::plugin()->translate('cat_prop_base_node_ilias_info'));
 		$te->setValue($this->origin->config()->getParentRefIdIfNoParentIdFound());
 		$this->addItem($te);
 
-		$te = new ilTextInputGUI(self::translate('cat_prop_base_node_external'), $this->conf(ICategoryOriginConfig::EXT_ID_NO_PARENT_ID_FOUND));
-		$te->setInfo(self::translate('cat_prop_base_node_external_info'));
+		$te = new ilTextInputGUI(self::plugin()->translate('cat_prop_base_node_external'), $this->conf(ICategoryOriginConfig::EXT_ID_NO_PARENT_ID_FOUND));
+		$te->setInfo(self::plugin()->translate('cat_prop_base_node_external_info'));
 		$te->setValue($this->origin->config()->getExternalParentIdIfNoParentIdFound());
 		$this->addItem($te);
 	}
@@ -43,18 +43,18 @@ class CategoryOriginConfigFormGUI extends OriginConfigFormGUI {
 	protected function addPropertiesNew() {
 		parent::addPropertiesNew();
 
-		$cb = new ilCheckboxInputGUI(self::translate('cat_prop_set_news'), $this->prop(CategoryOriginProperties::SHOW_NEWS));
+		$cb = new ilCheckboxInputGUI(self::plugin()->translate('cat_prop_set_news'), $this->prop(CategoryOriginProperties::SHOW_NEWS));
 		$cb->setChecked($this->origin->properties()->get(CategoryOriginProperties::SHOW_NEWS));
 		$this->addItem($cb);
 
-		$cb = new ilCheckboxInputGUI(self::translate('cat_prop_set_infopage'), $this->prop(CategoryOriginProperties::SHOW_INFO_TAB));
+		$cb = new ilCheckboxInputGUI(self::plugin()->translate('cat_prop_set_infopage'), $this->prop(CategoryOriginProperties::SHOW_INFO_TAB));
 		$cb->setChecked($this->origin->properties()->get(CategoryOriginProperties::SHOW_INFO_TAB));
 		$this->addItem($cb);
 	}
 
 
 	protected function addPropertiesUpdate() {
-		$cb = new ilCheckboxInputGUI(self::translate('cat_prop_move'), $this->prop(CategoryOriginProperties::MOVE_CATEGORY));
+		$cb = new ilCheckboxInputGUI(self::plugin()->translate('cat_prop_move'), $this->prop(CategoryOriginProperties::MOVE_CATEGORY));
 		$cb->setChecked($this->origin->properties()->get(CategoryOriginProperties::MOVE_CATEGORY));
 		$this->addItem($cb);
 
@@ -63,20 +63,20 @@ class CategoryOriginConfigFormGUI extends OriginConfigFormGUI {
 
 
 	protected function addPropertiesDelete() {
-		$delete = new ilRadioGroupInputGUI(self::translate('cat_prop_delete_mode'), $this->prop(CategoryOriginProperties::DELETE_MODE));
+		$delete = new ilRadioGroupInputGUI(self::plugin()->translate('cat_prop_delete_mode'), $this->prop(CategoryOriginProperties::DELETE_MODE));
 		$delete->setValue($this->origin->properties()->get(CategoryOriginProperties::DELETE_MODE));
 
-		$opt = new ilRadioOption(self::translate('cat_prop_delete_mode_none'), CategoryOriginProperties::DELETE_MODE_NONE);
+		$opt = new ilRadioOption(self::plugin()->translate('cat_prop_delete_mode_none'), CategoryOriginProperties::DELETE_MODE_NONE);
 		$delete->addOption($opt);
 
-		$opt = new ilRadioOption(self::translate('cat_prop_delete_mode_inactive', "", [ self::translate('com_prop_mark_deleted_text') ]), CategoryOriginProperties::DELETE_MODE_MARK);
+		$opt = new ilRadioOption(self::plugin()->translate('cat_prop_delete_mode_inactive', "", [ self::plugin()->translate('com_prop_mark_deleted_text') ]), CategoryOriginProperties::DELETE_MODE_MARK);
 		$delete->addOption($opt);
 
-		$te = new ilTextInputGUI(self::translate('cat_prop_delete_mode_inactive_text'), $this->prop(CategoryOriginProperties::DELETE_MODE_MARK_TEXT));
+		$te = new ilTextInputGUI(self::plugin()->translate('cat_prop_delete_mode_inactive_text'), $this->prop(CategoryOriginProperties::DELETE_MODE_MARK_TEXT));
 		$te->setValue($this->origin->properties()->get(CategoryOriginProperties::DELETE_MODE_MARK_TEXT));
 		$opt->addSubItem($te);
 
-		$opt = new ilRadioOption(self::translate('cat_prop_delete_mode_delete'), CategoryOriginProperties::DELETE_MODE_DELETE);
+		$opt = new ilRadioOption(self::plugin()->translate('cat_prop_delete_mode_delete'), CategoryOriginProperties::DELETE_MODE_DELETE);
 		$delete->addOption($opt);
 
 		$this->addItem($delete);
--- src/UI/ConfigFormGUI.php
+++ src/UI/ConfigFormGUI.php
@@ -43,8 +43,8 @@ class ConfigFormGUI extends ilPropertyFormGUI {
 		$this->config = $config;
 		$this->setFormAction(self::dic()->ctrl()->getFormAction($this->parent_gui));
 		$this->initForm();
-		$this->addCommandButton(hub2ConfigGUI::CMD_SAVE_CONFIG, self::translate('button_save'));
-		$this->addCommandButton(hub2ConfigGUI::CMD_CANCEL, self::translate('button_cancel'));
+		$this->addCommandButton(hub2ConfigGUI::CMD_SAVE_CONFIG, self::plugin()->translate('button_save'));
+		$this->addCommandButton(hub2ConfigGUI::CMD_CANCEL, self::plugin()->translate('button_cancel'));
 		parent::__construct();
 	}
 
@@ -53,91 +53,91 @@ class ConfigFormGUI extends ilPropertyFormGUI {
 	 *
 	 */
 	protected function initForm() {
-		$this->setTitle(self::translate('admin_form_title'));
+		$this->setTitle(self::plugin()->translate('admin_form_title'));
 
-		$item = new ilTextInputGUI(self::translate('admin_origins_path'), IHubConfig::ORIGIN_IMPLEMENTATION_PATH);
-		$item->setInfo(self::translate('admin_origins_path_info'));
+		$item = new ilTextInputGUI(self::plugin()->translate('admin_origins_path'), IHubConfig::ORIGIN_IMPLEMENTATION_PATH);
+		$item->setInfo(self::plugin()->translate('admin_origins_path_info'));
 		$item->setValue($this->config->get(IHubConfig::ORIGIN_IMPLEMENTATION_PATH));
 		$this->addItem($item);
 
-		$cb = new ilCheckboxInputGUI(self::translate('admin_lock'), IHubConfig::LOCK_ORIGINS_CONFIG);
+		$cb = new ilCheckboxInputGUI(self::plugin()->translate('admin_lock'), IHubConfig::LOCK_ORIGINS_CONFIG);
 		$cb->setChecked($this->config->get(IHubConfig::LOCK_ORIGINS_CONFIG));
 		$this->addItem($cb);
 
 		$item = new ilFormSectionHeaderGUI();
-		$item->setTitle(self::translate('common_permissions'));
+		$item->setTitle(self::plugin()->translate('common_permissions'));
 		$this->addItem($item);
 
-		$item = new ilTextInputGUI(self::translate('common_roles'), IHubConfig::ADMINISTRATE_HUB_ROLE_IDS);
+		$item = new ilTextInputGUI(self::plugin()->translate('common_roles'), IHubConfig::ADMINISTRATE_HUB_ROLE_IDS);
 		$item->setValue($this->config->get(IHubConfig::ADMINISTRATE_HUB_ROLE_IDS));
-		$item->setInfo(self::translate('admin_roles_info'));
+		$item->setInfo(self::plugin()->translate('admin_roles_info'));
 		$this->addItem($item);
 
 		$h = new ilFormSectionHeaderGUI();
-		$h->setTitle(self::translate('admin_shortlink'));
+		$h->setTitle(self::plugin()->translate('admin_shortlink'));
 		$this->addItem($h);
 
-		$item = new ilTextAreaInputGUI(self::translate('admin_msg_'
+		$item = new ilTextAreaInputGUI(self::plugin()->translate('admin_msg_'
 			. IHubConfig::SHORTLINK_OBJECT_NOT_FOUND), IHubConfig::SHORTLINK_OBJECT_NOT_FOUND);
 		$item->setUseRte(false);
 		$item->setValue($this->config->get(IHubConfig::SHORTLINK_OBJECT_NOT_FOUND));
-		$item->setInfo(self::translate('admin_msg_' . IHubConfig::SHORTLINK_OBJECT_NOT_FOUND . '_info'));
+		$item->setInfo(self::plugin()->translate('admin_msg_' . IHubConfig::SHORTLINK_OBJECT_NOT_FOUND . '_info'));
 		$this->addItem($item);
 
-		$item = new ilTextAreaInputGUI(self::translate('admin_msg_'
+		$item = new ilTextAreaInputGUI(self::plugin()->translate('admin_msg_'
 			. IHubConfig::SHORTLINK_OBJECT_NOT_ACCESSIBLE), IHubConfig::SHORTLINK_OBJECT_NOT_ACCESSIBLE);
 		$item->setUseRte(false);
 		$item->setValue($this->config->get(IHubConfig::SHORTLINK_OBJECT_NOT_ACCESSIBLE));
-		$item->setInfo(self::translate('admin_msg_' . IHubConfig::SHORTLINK_OBJECT_NOT_ACCESSIBLE . '_info'));
+		$item->setInfo(self::plugin()->translate('admin_msg_' . IHubConfig::SHORTLINK_OBJECT_NOT_ACCESSIBLE . '_info'));
 		$this->addItem($item);
 
-		$item = new ilTextAreaInputGUI(self::translate('admin_msg_' . IHubConfig::SHORTLINK_SUCCESS), IHubConfig::SHORTLINK_SUCCESS);
+		$item = new ilTextAreaInputGUI(self::plugin()->translate('admin_msg_' . IHubConfig::SHORTLINK_SUCCESS), IHubConfig::SHORTLINK_SUCCESS);
 		$item->setUseRte(false);
 		$item->setValue($this->config->get(IHubConfig::SHORTLINK_SUCCESS));
-		$item->setInfo(self::translate('admin_msg_' . IHubConfig::SHORTLINK_SUCCESS . '_info'));
+		$item->setInfo(self::plugin()->translate('admin_msg_' . IHubConfig::SHORTLINK_SUCCESS . '_info'));
 		$this->addItem($item);
 
 		//		$h = new ilFormSectionHeaderGUI();
-		//		$h->setTitle(self::translate('admin_membership'));
+		//		$h->setTitle(self::plugin()->translate('admin_membership'));
 		//		$this->addItem($h);
 		//
-		//		$cb = new ilCheckboxInputGUI(self::translate('admin_membership_activate'), hubCOnfig::F_MMAIL_ACTIVE);
-		//		$cb->setInfo(self::translate('admin_membership_activate_info'));
+		//		$cb = new ilCheckboxInputGUI(self::plugin()->translate('admin_membership_activate'), hubCOnfig::F_MMAIL_ACTIVE);
+		//		$cb->setInfo(self::plugin()->translate('admin_membership_activate_info'));
 		//		$this->addItem($cb);
 		//
-		//		$mm = new ilTextInputGUI(self::translate('admin_membership_mail_subject'), hubConfig::F_MMAIL_SUBJECT);
-		//		$mm->setInfo(self::translate('admin_membership_mail_subject_info'));
+		//		$mm = new ilTextInputGUI(self::plugin()->translate('admin_membership_mail_subject'), hubConfig::F_MMAIL_SUBJECT);
+		//		$mm->setInfo(self::plugin()->translate('admin_membership_mail_subject_info'));
 		//		$this->addItem($mm);
 		//
-		//		$mm = new ilTextAreaInputGUI(self::translate('admin_membership_mail_msg'), hubConfig::F_MMAIL_MSG);
-		//		$mm->setInfo(self::translate('admin_membership_mail_msg_info'));
+		//		$mm = new ilTextAreaInputGUI(self::plugin()->translate('admin_membership_mail_msg'), hubConfig::F_MMAIL_MSG);
+		//		$mm->setInfo(self::plugin()->translate('admin_membership_mail_msg_info'));
 		//		$this->addItem($mm);
 		//
 		//		$h = new ilFormSectionHeaderGUI();
-		//		$h->setTitle(self::translate('admin_user_creation'));
+		//		$h->setTitle(self::plugin()->translate('admin_user_creation'));
 		//		$this->addItem($h);
 		//
-		//		$ti = new ilTextInputGUI(self::translate('admin_user_creation_standard_role'), hubConfig::F_STANDARD_ROLE);
+		//		$ti = new ilTextInputGUI(self::plugin()->translate('admin_user_creation_standard_role'), hubConfig::F_STANDARD_ROLE);
 		//		$this->addItem($ti);
 		//
 		//		$h = new ilFormSectionHeaderGUI();
-		//		$h->setTitle(self::translate('admin_header_sync'));
+		//		$h->setTitle(self::plugin()->translate('admin_header_sync'));
 		//		$this->addItem($h);
 		//
-		//		$cb = new ilCheckboxInputGUI(self::translate('admin_use_async'), hubConfig::F_USE_ASYNC);
-		//		$cb->setInfo(self::translate('admin_use_async_info'));
+		//		$cb = new ilCheckboxInputGUI(self::plugin()->translate('admin_use_async'), hubConfig::F_USE_ASYNC);
+		//		$cb->setInfo(self::plugin()->translate('admin_use_async_info'));
 		//
-		//		$te = new ilTextInputGUI(self::translate('admin_async_user'), hubConfig::F_ASYNC_USER);
+		//		$te = new ilTextInputGUI(self::plugin()->translate('admin_async_user'), hubConfig::F_ASYNC_USER);
 		//		$cb->addSubItem($te);
-		//		$te = new ilTextInputGUI(self::translate('admin_async_password'), hubConfig::F_ASYNC_PASSWORD);
+		//		$te = new ilTextInputGUI(self::plugin()->translate('admin_async_password'), hubConfig::F_ASYNC_PASSWORD);
 		//		$cb->addSubItem($te);
-		//		$te = new ilTextInputGUI(self::translate('admin_async_client'), hubConfig::F_ASYNC_CLIENT);
+		//		$te = new ilTextInputGUI(self::plugin()->translate('admin_async_client'), hubConfig::F_ASYNC_CLIENT);
 		//		$cb->addSubItem($te);
-		//		$te = new ilTextInputGUI(self::translate('admin_async_cli_php'), hubConfig::F_ASYNC_CLI_PHP);
+		//		$te = new ilTextInputGUI(self::plugin()->translate('admin_async_cli_php'), hubConfig::F_ASYNC_CLI_PHP);
 		//		$cb->addSubItem($te);
 		//		$this->addItem($cb);
 
-		//		$cb = new ilCheckboxInputGUI(self::translate('admin_import_export'), hubConfig::F_IMPORT_EXPORT);
+		//		$cb = new ilCheckboxInputGUI(self::plugin()->translate('admin_import_export'), hubConfig::F_IMPORT_EXPORT);
 		//		$this->addItem($cb);
 	}
 }
