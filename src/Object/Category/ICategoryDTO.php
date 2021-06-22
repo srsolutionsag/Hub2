335111c15c159c59ae2aae429e68d7203ee90b5c
Lukas Zehnder <lz@studer-raimann.ch>
2021-06-22 07:34:39 +0200

    Added possibility to declare sorting direction and use manual sorting for categories

--- src/Object/Category/ICategoryDTO.php
+++ src/Object/Category/ICategoryDTO.php
@@ -22,6 +22,17 @@ interface ICategoryDTO extends IDataTransferObject, ITaxonomyAndMetadataAwareDat
     const ORDER_TYPE_ACTIVATION = 2;
     const ORDER_TYPE_INHERIT = 3;
     const ORDER_TYPE_CREATION = 4;
+
+    const ORDER_DIRECTION_ASC = 0;
+    const ORDER_DIRECTION_DESC = 1;
+
+    const ORDER_NEW_ITEMS_POSITION_TOP = 0;
+    const ORDER_NEW_ITEMS_POSITION_BOTTOM = 1;
+
+    const ORDER_NEW_ITEMS_BY_TITLE = 0;
+    const ORDER_NEW_ITEMS_BY_CREATION = 1;
+    const ORDER_NEW_ITEMS_BY_ACTIVATION = 2;
+
     const PARENT_ID_TYPE_REF_ID = 1;
     const PARENT_ID_TYPE_EXTERNAL_EXT_ID = 2;
 }
