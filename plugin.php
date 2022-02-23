cfa960dc980f2c6150d862b5c0989db002b552fe
Fabian Schmid <fabian@sr.solutions>
2022-02-23 08:18:50 +0100

    plugin version

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/srag/dic/src/PHPVersionChecker.php";
 
 $id = 'hub2';
-$version = '2.6.0';
+$version = '2.7.0';
 $ilias_min_version = '5.4.0';
 $ilias_max_version = '6.999';
 $responsible = 'sr solutions';
