5a02de52a480c3070da12e21991310224d9b7dd1
Fabian Schmid <fabian@sr.solutions>
2022-09-22 07:46:51 +0200

    Prepared new Plugin Version

--- plugin.php
+++ plugin.php
@@ -3,9 +3,9 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 $id = 'hub2';
-$version = '3.0.1';
+$version = '3.0.2';
 $ilias_min_version = '5.4.0';
 $ilias_max_version = '7.999';
-$responsible = 'sr solutions';
+$responsible = 'sr.solutions';
 $responsible_mail = 'support@sr.solutions';
 $supports_cli_setup = false;
