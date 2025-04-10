8be38b4e2f7382017d2053f0f0af63db0538baea
Fabian Schmid <fabian@sr.solutions>
2025-04-10 11:06:54 +0200

    [RELEASE] Version v5.0.0

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 $id = 'hub2';
-$version = '4.7.0';
+$version = '5.0.0';
 $ilias_min_version = '9.0';
 $ilias_max_version = '9.999';
 $responsible = 'sr.solutions';
